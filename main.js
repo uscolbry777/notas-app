4a530e97cfd03df8b48a2d4efbdd1f5043b8f81d
uscolbry777 <BRAYITAN.TALLER@GMAIL.COM>
2025-03-08 13:39:59 -0500

    main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Forzar actualización en GitHub Pages - Versión 2
 const API_URL = "https://api.jsonbin.io/v3/b";
 const MASTER_KEY = "$2a$10$Y3NKboje6Op54dQnjKdQvu0f08ZABCeJeO4WVk1RNEJyQsfGwot7."; // Reemplaza con tu X-Master-Key
 const ACCESS_KEY = "$2a$10$j9GP4KOPTkoSmpw3k9YXeOvB6vP/nOmXlbUEr6DVJQUheqZziDBEu"; // Reemplaza con tu X-Access-Key
